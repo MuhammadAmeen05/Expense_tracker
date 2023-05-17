0996b14ab9045de14d3cd007d388a25f27f2cb8d
Your Name <muhammadameenbirahmani6@gmail.com>
2023-05-18 00:34:41 +0500

    updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const colors=require('colors');
 const dotenv=require('dotenv')
+const path=require('path')
 const morgan=require('morgan')
 const app=express();
 const ConnectDB=require('./conn/db')
@@ -9,11 +10,16 @@ app.use(express.json())
 const transcations=require('./routes/transcations')
 //console.log(transcations)
 ConnectDB();
-if(process.env.NODE_ENV ==='development')
-{
-  app.use(morgan('dev'))
-}
+// if(process.env.NODE_ENV ==='development')
+// {
+//   app.use(morgan('dev'))
+// }
 app.use('/api/v1/transcations',transcations)
+
+app.use(express.static(path.join(__dirname,'/client/build')))
+app.get("*",function(req,res){
+    res.sendFile(path.join(__dirname,'/client/build/index.html'))
+})
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`.yellow.bold)
   })
